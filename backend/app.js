091b4ed20fb543f7f1d7ad3651504775d202c636
Erasmo Cardoso <erasmo@vmi-informatica.com.br>
2023-06-20 16:27:05 -0300

    update

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,16 @@ const express = require ("express")
 const app = express()
 const port = 9000
 
+app.get('/backend/conectfront', (req, res) => {
+    
+    const data = [
+      { id: 1, name: 'Usuário 1' },
+      { id: 2, name: 'Usuário 2' },
+      { id: 3, name: 'Usuário 3' },
+    ];
+  
+    res.json(data);
+  });
 
 app.listen(port , () => {
     console.log("Conectado porta 9000")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/backend/conectfront');
+        setData(response.data);
+      } catch (error) {
+        console.error('Erro ao conectar no Backend', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lista de Consultas..</h1>
+        <ul>
+          {data.map((user) => (
+            <React.Fragment key={user.id}>
+              <li>{user.name}</li>
+            </React.Fragment>
+          ))}
+        </ul>
       </header>
     </div>
   );
