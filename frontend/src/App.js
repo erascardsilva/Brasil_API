c0b9d87091de3216a5674c983db27524e6c99b94
Erasmo Cardoso <erasmo@vmi-informatica.com.br>
2023-06-23 19:23:32 -0300

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,31 +39,38 @@ function App() {
   return (
     <div className="App">
       <div id="item-0">
-        <img width={160} src="./images/apilogo.png" alt="Logo"></img>
-        
-        <img width={160} src="./images/apilogo.png" alt="Logo"></img><p />
-        
-        <button width={ 50 }>Bancos</button> <button width={ 50 } >Nomes</button> 
-        <button width={ 50 } >Endereços</button> <button width={ 50 } >Dolar</button>
-        <hr />
+        <div class="logo-container">
+          <img width="160" src="./images/apilogo.png" alt="Logo" />
+          
+        </div>
+        <div class="button-container">
+          <button className="custom-button">Bancos</button>
+          <button className="custom-button">Nomes</button>
+          <button className="custom-button">Endereços</button>
+          <button className="custom-button">Dolar</button>
+        </div>
       </div>
+
+
       <div id="item-1">
         <center><h3>Brasil API consultas</h3></center> <hr />
-        <center><h4>Digite o codigo bancario</h4></center> 
+        <center><h4>Digite o codigo bancario</h4></center>
         <center>
-        <form onSubmit={handleSubmit} id="busca">
-          <input
-            type="text"
-            value={formData}
-            onChange={(e) => setFormData(e.target.value)}
-            placeholder="Digite o nome"
-          />
-         <center> <button type="submit">Buscar Banco</button> </center>
-        </form></center>
+          <form onSubmit={handleSubmit} id="busca">
+            <input
+              type="text"
+              value={formData}
+              onChange={(e) => setFormData(e.target.value)}
+              placeholder="Digite o nome"
+            />
+            <center> <button type="submit">Buscar Banco</button> </center>
+          </form>
+        </center>
         <hr />
-        
+
         <p id="connection-status">{connectionStatus}</p>
       </div>
+
       <div id="item-2">
         <div className="App">
           <header className="App-header">
@@ -71,13 +78,12 @@ function App() {
               {data.map((bank) => (
                 <React.Fragment key={bank.id}>
                   <h3>Número: {bank.code}</h3>
-                  <h3>ISPB : {bank.ispb}</h3>
+                  <h3>ISPB: {bank.ispb}</h3>
                   <hr />
                   <h3>Nome do Banco:</h3>
                   <h3>{bank.name}</h3>
                   <hr />
                   <h3>{bank.fullName}</h3>
-                  
                   <hr />
                 </React.Fragment>
               ))}
