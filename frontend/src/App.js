3dafb27aa0017804f85a045e58711e7e6ed96879
Erasmo Cardoso <erasmo@vmi-informatica.com.br>
2023-06-23 21:32:36 -0300

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import logo from './images/logo.png';
+import logo from "./images/logo.png" ;
 
 function App() {
   const [data, setData] = useState([]);
